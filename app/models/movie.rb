41f511e84781455f84f5a1f687397ab555919831
Erik Luebbers <erluebbers@gmail.com>
2022-03-05 12:02:40 -0800

    commit tree b3a1c84d9ed9bf124b7c0d3df4cfb5facb3b3c79

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Movie < ActiveRecord::Base
 
   def self.create_with_title(title)
@@ -20,11 +22,28 @@ class Movie < ActiveRecord::Base
     self.find_by(id: id)
   end
 
-  def self.find_movie_with_attributes(title, release_date)
-    self.find_by(title: title, release_date: release_date)
+  def self.find_movie_with_attributes(hash)
+    self.find_by(title: hash[:title], release_date: hash[:release_date])
   end
 
+  def self.find_movies_after_2002
+    self.where("release_date > 2002")
+  end
 
+  def update_with_attributes(attributes)
+    self.update(attributes)
+  end
 
+  def self.update_all_titles(title)
+    self.update(title: title)
+  end
+
+  def self.delete_by_id(id)
+    self.find(id).destroy
+  end
+
+  def self.delete_all_movies
+    self.destroy_all
+  end
 
 end

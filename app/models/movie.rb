a42b403458a46035bdb796f85c6979a592b20e35
Erik Luebbers <erluebbers@gmail.com>
2022-03-05 10:23:54 -0800

    Done.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,30 @@
 class Movie < ActiveRecord::Base
 
+  def self.create_with_title(title)
+    self.create(title: title)
+  end
+
+  def self.first_movie
+    self.first
+  end
+
+  def self.last_movie
+    self.last 
+  end
+
+  def self.movie_count
+    self.count 
+  end
+
+  def self.find_movie_with_id(id)
+    self.find_by(id: id)
+  end
+
+  def self.find_movie_with_attributes(title, release_date)
+    self.find_by(title: title, release_date: release_date)
+  end
+
+
+
+
 end
